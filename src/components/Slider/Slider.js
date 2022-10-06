ffb2f3fa44e294017e27addae37024bed42b0f79
Oleg <hellreebok@gmail.com>
2022-10-06 20:05:21 +0300

    update import images

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,32 +2,33 @@ import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
 
-import firstImg from '../../images/slider/slider1.jpg';
-import secondImg from '../../images/slider/slider2.jpg';
-import thirdImg from '../../images/slider/slider6.jpg';
+// import firstImg from '../../images/slider/slider1.jpg';
+// import secondImg from '../../images/slider/slider2.jpg';
+// import thirdImg from '../../images/slider/slider6.jpg';
+import images from 'images/slider';
 import s from './Slider.module.css';
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const slider = () => (
-  <AutoplaySlider
-    play={false}
-    cancelOnInteraction={false} // should stop playing on user interaction
-    interval={3000}
-    bullets={false}
-    mobileTouch={true}
-    className={s.slider}
-  >
-    <div data-src={secondImg} className={s.img}>
-      <p>This is info for first slider</p>
-    </div>
-    <div data-src={firstImg} className={s.img}>
-      <p>This is info for second slider</p>
-    </div>
-    <div data-src={thirdImg} className={s.img}>
-      <p>This is info for third slider</p>
-    </div>
-  </AutoplaySlider>
+    <AutoplaySlider
+        play={false}
+        cancelOnInteraction={false} // should stop playing on user interaction
+        interval={3000}
+        bullets={false}
+        mobileTouch={true}
+        className={s.slider}
+    >
+        <div data-src={images.img1} className={s.img}>
+            <p>This is info for first slider</p>
+        </div>
+        <div data-src={images.img2} className={s.img}>
+            <p>This is info for second slider</p>
+        </div>
+        <div data-src={images.img6} className={s.img}>
+            <p>This is info for third slider</p>
+        </div>
+    </AutoplaySlider>
 );
 
 export default slider;
---
+++ src/images/slider/index.js
@@ -0,0 +1,6 @@
+import img1 from './slider1.jpg';
+import img2 from './slider2.jpg';
+import img6 from './slider6.jpg';
+
+const images = { img1, img2, img6 };
+export default images;
