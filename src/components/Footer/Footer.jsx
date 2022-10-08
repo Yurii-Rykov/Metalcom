1b4ad4a65042f4cbd9f4984b4790e19c509dc153
Oleg <hellreebok@gmail.com>
2022-10-08 10:24:21 +0300

    add localization

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,78 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Logo } from 'components';
-import icons from '../../images/icons'
+import icons from '../../images/icons';
 import s from './Footer.module.css';
 
-
 const Footer = () => {
+    const lang = useSelector(state => state.lang);
+
     return (
         <footer className={s.footer} id="footer">
             <NavLink to="/" className={s.wrapper_logo}>
-                <Logo className={s.logoFooter}/>
+                <Logo className={s.logoFooter} />
                 <p className={s.name}>METALKOM</p>
             </NavLink>
 
             <ul className={s.social}>
-                <li className={s.social_item}><a href="/" className={s.social_link}><img className={s.link_instagram} src={`${icons.instagram_icon}`} alt="icon_instagram" /></a></li>
-                <li className={s.social_item}><a href="/" className={s.social_link}><img className={s.link_facebook} src={`${icons.facebook_icon}`} alt="icon_facebook" /></a></li>
-                <li className={s.social_item}><a href="/" className={s.social_link}><img className={s.link_email} src={`${icons.email_icon}`} alt="icon_email" /> </a></li>
+                <li className={s.social_item}>
+                    <a href="/" className={s.social_link}>
+                        <img className={s.link_instagram} src={`${icons.instagram_icon}`} alt="icon_instagram" />
+                    </a>
+                </li>
+                <li className={s.social_item}>
+                    <a href="/" className={s.social_link}>
+                        <img className={s.link_facebook} src={`${icons.facebook_icon}`} alt="icon_facebook" />
+                    </a>
+                </li>
+                <li className={s.social_item}>
+                    <a href="/" className={s.social_link}>
+                        <img className={s.link_email} src={`${icons.email_icon}`} alt="icon_email" />{' '}
+                    </a>
+                </li>
             </ul>
 
             <ul className={s.about}>
-                <li className={s.about_item}><p className={s.about_text}>Інформація:</p></li>
-                <li className={s.about_item}><NavLink to="/catalog" className={s.about_nav}>Умови оплати</NavLink></li>
-                <li className={s.about_item}><NavLink to="/#about" className={s.about_nav}>Доставка</NavLink></li>
-                <li className={s.about_item}><NavLink to="/cooperation" className={s.about_nav}>Гарантія на товар</NavLink></li>
+                <li className={s.about_item}>
+                    <p className={s.about_text}>{lang.footerInfo}</p>
+                </li>
+                <li className={s.about_item}>
+                    <NavLink to="/catalog" className={s.about_nav}>
+                        {lang.footerCondition}
+                    </NavLink>
+                </li>
+                <li className={s.about_item}>
+                    <NavLink to="/#about" className={s.about_nav}>
+                        {lang.footerDelivery}
+                    </NavLink>
+                </li>
+                <li className={s.about_item}>
+                    <NavLink to="/cooperation" className={s.about_nav}>
+                        {lang.footerGuarantee}
+                    </NavLink>
+                </li>
             </ul>
-            
+
             <ul className={s.wrapper}>
-                <li className={s.wrapper_item}><a href='tel:+380683526670' className={s.wrapper_contacts}><img src={`${icons.phone_icon}`} alt="icon_phone" /><p className={s.wrapper_text}>+38 068 352 66 70</p></a></li>
-                <li className={s.wrapper_item}><a href='mailto:m22070202@gmail.com' className={s.wrapper_contacts}><img src={`${icons.secondEmail_icon}`} alt="icon_email" /><p className={s.wrapper_text}>m22070202@gmail.com</p></a></li>
-                <li className={s.wrapper_item}><a href='https://goo.gl/maps/6PcpeWitLvjP28Yv9' className={s.wrapper_contacts}><img src={`${icons.location_icon}`} alt="icon_email" /><p className={s.wrapper_text}>М.Київ вул. Бережанська 4</p></a></li>
+                <li className={s.wrapper_item}>
+                    <a href="tel:+380683526670" className={s.wrapper_contacts}>
+                        <img src={`${icons.phone_icon}`} alt="icon_phone" />
+                        <p className={s.wrapper_text}>+38 068 352 66 70</p>
+                    </a>
+                </li>
+                <li className={s.wrapper_item}>
+                    <a href="mailto:m22070202@gmail.com" className={s.wrapper_contacts}>
+                        <img src={`${icons.secondEmail_icon}`} alt="icon_email" />
+                        <p className={s.wrapper_text}>m22070202@gmail.com</p>
+                    </a>
+                </li>
+                <li className={s.wrapper_item}>
+                    <a href="https://goo.gl/maps/6PcpeWitLvjP28Yv9" className={s.wrapper_contacts}>
+                        <img src={`${icons.location_icon}`} alt="icon_email" />
+                        <p className={s.wrapper_text}>{lang.adress}</p>
+                    </a>
+                </li>
             </ul>
-            
-   
         </footer>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,60 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Logo } from 'components';
 import s from './Header.module.css';
+import { languageSelection } from 'redux/reducers';
 
 const Header = () => {
+    const [currentLang, setCurrentLang] = useState('ua');
+    const lang = useSelector(state => state.lang);
+    const dispatch = useDispatch();
+    const setLang = lang => {
+        setCurrentLang(lang);
+        dispatch(languageSelection(lang));
+    };
+
     return (
         <div className={s.header}>
             <NavLink to="/" className={s.wrapper_logo}>
-                <Logo className={s.logoHeader}/>
+                <Logo className={s.logoHeader} />
                 <h1 className={s.name}>METALKOM</h1>
             </NavLink>
-            <NavLink to="/" className={s.nav}>Головна</NavLink>
-            <NavLink to="/catalog" className={s.nav}>Каталог</NavLink>
-            <NavLink to="/#about" className={s.nav}>Про компанію</NavLink>
-            <NavLink to="/cooperation" className={s.nav}>Співробітництво</NavLink>
-            <NavLink to="#footer" className={s.nav}>Контакти</NavLink>
+            <nav>
+                <NavLink to="/" className={s.nav}>
+                    {lang.head}
+                </NavLink>
+                <NavLink to="/catalog" className={s.nav}>
+                    {lang.catalog}
+                </NavLink>
+                <NavLink to="/#about" className={s.nav}>
+                    {lang.about}
+                </NavLink>
+                <NavLink to="/cooperation" className={s.nav}>
+                    {lang.cooperation}
+                </NavLink>
+                <NavLink to="#footer" className={s.nav}>
+                    {lang.contacts}
+                </NavLink>
+            </nav>
+
+            <div className={s.localization}>
+                <button
+                    className={currentLang === 'ua' ? s.active : s.lang}
+                    type="button"
+                    onClick={() => setLang('ua')}
+                >
+                    UA
+                </button>
+                <span> / </span>
+                <button
+                    className={currentLang === 'ru' ? s.active : s.lang}
+                    type="button"
+                    onClick={() => setLang('ru')}
+                >
+                    RU
+                </button>
+            </div>
         </div>
     );
 };
